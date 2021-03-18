4feb7c05657ebc17271da04eb4fc4f43a664c49c
SunEom <dkxl0701@gmail.com>
2021-03-19 01:22:36 +0900

    Fixed Header UI

--- src/components/Header/HeaderPresenter.jsx
+++ src/components/Header/HeaderPresenter.jsx
@@ -6,19 +6,19 @@ const HeaderPresenter = () => {
     <header className="font-alfa w-full">
       {/* Top Part */}
       <div
-        className="flex justify-between py-1 px-20 lg:px-52 whitespace-nowrap border-solid border-b border-gray-300"
-        style={{ height: 35, minWidth: 1700 }}
+        className="flex justify-between py-1 px-52 whitespace-nowrap border-solid border-b border-gray-300"
+        style={{ height: 35, width: 1700 }}
       >
         <div className="pr-80">
-          <Link to="1" className="text-black px-5 text-base md:text-lg font-bold border-solid border-r border-gray-300 ">
+          <Link to="1" className="text-black px-5 text-lg font-bold border-solid border-r border-gray-300 ">
             <span>Z</span>
             <span className="text-sm">-RT</span>
           </Link>
-          <Link to="1" className="text-mainRed px-5 text-base md:text-lg font-bold  border-solid border-r border-gray-300  ">
+          <Link to="1" className="text-mainRed px-5 text-lg font-bold  border-solid border-r border-gray-300  ">
             <span>XYZ</span>
             <span className="text-sm">-MART</span>
           </Link>
-          <Link to="1" className="text-black px-5 text-base md:text-lg font-bold">
+          <Link to="1" className="text-black px-5 text-lg font-bold">
             <span>XYZ</span>
             <span className="text-sm">-MART</span>
           </Link>
@@ -41,8 +41,8 @@ const HeaderPresenter = () => {
 
       {/* Middle Part */}
       <div
-        className="w-full justify-between py-1 whitespace-nowrap flex items-center box-border px-20 lg:px-52"
-        style={{ height: 130, minWidth: 1700 }}
+        className="w-full justify-between py-1 whitespace-nowrap flex items-center box-border px-52"
+        style={{ height: 130, width: 1310 }}
       >
         <Link to="/" className=" px-5 py-10 text-3xl text-extrabold text-mainRed flex items-center box-sizing ">
           <span>XYZ</span>
@@ -87,8 +87,8 @@ const HeaderPresenter = () => {
 
       {/* Bottom Part */}
       <div
-        className="h-12 min-w-full bg-mainRed flex justify-between py-1 px-4 sm:px-20 lg:px-52 whitespace-nowrap font-mont"
-        style={{ height: 50, minWidth: 1700 }}
+        className="h-12 min-w-full bg-mainRed flex justify-between py-1 px-52 whitespace-nowrap font-mont"
+        style={{ height: 50, width: 1700 }}
       >
         <div className="flex items-center pr-20">
           <Link to="1" className="text-mainYellow px-5 text-base md:text-md font-medium uppercase ">
