bd3f3a356b81daa1a429e94b3c1f99cfbc1eaf2c
SunEom <dkxl0701@gmail.com>
2021-03-18 18:59:07 +0900

    Created Medium size Item component

---
+++ src/components/MItem/ItemPresenter.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const ItemPresenter = ({ img, company, serial, cost, isNew, id }) => {
+  return (
+    <Link to={`/${id}`}>
+      <div classname="w-full flex justify-center">
+        <img src={img} alt="" style={{ margin: 0 }} />
+      </div>
+      <div className="h-20 flex flex-col justify-center ">
+        <div className="font-bold py-2">{company}</div>
+        <div className="uppercase">{serial}</div>
+      </div>
+      <div className="h-auto flex flex-col justify-center ">
+        <div className="font-bold py-2 text-lg">{cost}원</div>
+        {isNew ? <div className="uppercase border border-solid w-12 text-center font-bold text-sm">new</div> : null}
+      </div>
+    </Link>
+  );
+};
+
+export default ItemPresenter;
