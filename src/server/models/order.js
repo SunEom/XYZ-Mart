40cd305ec89bccfac2c2a5b6e2e6478f66d98b14
SunEom <dkxl0701@gmail.com>
2021-03-28 16:27:40 +0900

    Created Order, Cart models

---
+++ src/server/models/order.js
@@ -0,0 +1,38 @@
+const Sequelize = require('sequelize');
+
+module.exports = class Order extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        user: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        product: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        size: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.NOW,
+        },
+      },
+      {
+        sequelize,
+        timestamps: false,
+        underscored: false,
+        modelName: 'ORDERS',
+        tableName: 'orders',
+        paranoid: false,
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+      }
+    );
+  }
+  static associate(db) {}
+};
